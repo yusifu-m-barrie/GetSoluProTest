c6ab40b1c064a122cd63f93aa4ceb91b712da961
yusifu-m-barrie <usifubarriem@gmail.com>
2021-08-02 13:28:17 -0700

    adding menus in the index.php file

--- index.php
+++ index.php
@@ -27,11 +27,45 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
      margin-right: auto;
      width: 30%;
     }
+
+    .topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
     </style>
-    
+
 </head>
 <body>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="reset-password.php">Reset Your Password</a>
+  <a href="logout.php">Sign Out of Your Account</a>
+</div>
 
+<h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to my Test site.</h1>
+    
 <br><br>
 <img src="" id="img" class="center">
 
@@ -43,17 +77,11 @@ Type in a URL to an image below :
 <input type="text" id="url"/><input type="button" id="btn" value="show image" />
 
 
-
-
 <script>
     $('#btn').on('click', function() {
 $('#img').attr('src', $('#url').val());
 });
 </script>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to my Test site.</h1>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
+    
 </body>
 </html>
